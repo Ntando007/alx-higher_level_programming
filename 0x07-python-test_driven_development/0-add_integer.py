84d6df3ad36003633b6ecc700225cdc5280251b4
JoseMarulanda <josejumarumol@gmail.com>
2019-10-05 19:51:32 +0000

    fix

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -10,12 +10,8 @@ def add_integer(a, b=98):
     it casts them, it returns the sum of a and b.
     """
 
-    if not isinstance(a, (int, float)):
+    if a is None or (type(a) is not int and type(a) is not float):
         raise TypeError("a must be an integer")
-    if not isinstance(b, (int, float)):
+    if type(b) is not int and type(b) is not float:
         raise TypeError("b must be an integer")
-    if isinstance(a, float):
-        a = int(a)
-    if isinstance(b, float):
-        b = int(b)
-    return a + b
+    return int(a) + int(b)
