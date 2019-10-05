297a8c35a58b3d8d911511bb0b31581d8497b8ba
JoseMarulanda <josejumarumol@gmail.com>
2019-10-05 19:33:01 +0000

    fix

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -10,11 +10,12 @@ def add_integer(a, b=98):
     it casts them, it returns the sum of a and b.
     """
 
-    try:
-        if isinstance(a, (int, float)) is False:
-            raise TypeError('a must be an integer')
-        elif isinstance(b, (int, float)) is False:
-            raise TypeError('b must be an integer')
-        return(int(a) + int(b))
-    except:
-        raise
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
+    if not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
+    if isinstance(a, float):
+        a = int(a)
+    if isinstance(b, float):
+        b = int(b)
+    return a + b
