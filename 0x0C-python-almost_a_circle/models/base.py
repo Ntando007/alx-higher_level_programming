93df59bec649b682328959981bc3ff8cc382bd13
JoseMarulanda <josejumarumol@gmail.com>
2019-10-24 22:03:11 +0000

    fixed functions

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -35,25 +35,26 @@ class Base:
     def save_to_file(cls, list_objs):
         """writes the JSON string representation
         of list_objs to a file"""
+        filename = "{}.json".format(cls.__name__)
+        list_dic = []
 
-        list_objects = []
-        if list_objs is None or len(list_objs) is 0:
-            list_objects = []
+        if not list_objs:
+            pass
         else:
-            for i in list_objs:
-                list_objects.append(i.to_dictionary())
-        json_string = Base.to_json_string(list_objects)
-        with open("{}.json".format(cls.__name__), mode='w', encoding='utf-8')\
-                as file:
-            file.write(json_string)
+            for i in range(len(list_objs)):
+                list_dic.append(list_objs[i].to_dictionary())
+
+        lists = cls.to_json_string(list_dic)
+
+        with open(filename, 'w') as f:
+            f.write(lists)
 
     @staticmethod
     def from_json_string(json_string):
         """returns the list of the JSON
         string representation json_string
         """
-
-        if json_string is None or json_string is "":
+        if not json_string:
             return []
         return json.loads(json_string)
 
@@ -75,14 +76,31 @@ class Base:
     @classmethod
     def load_from_file(cls):
         """returns a list of instances"""
+        filename = "{}.csv".format(cls.__name__)
 
-        instance_list = []
-        filename = "{}.json".format(cls.__name__)
-        if os.path.isfile(filename):
-            with open(filename) as f:
-                instance_object = cls.from_json_string(f.read())
-                for instance_dict in instance_object:
-                    instance_list.append(cls.create(**instance_dict))
-                return instance_list
-        else:
+        if os.path.exists(filename) is False:
             return []
+
+        with open(filename, 'r') as readFile:
+            reader = csv.reader(readFile)
+            csv_list = list(reader)
+
+        if cls.__name__ == "Rectangle":
+            list_keys = ['id', 'width', 'height', 'x', 'y']
+        else:
+            list_keys = ['id', 'size', 'x', 'y']
+
+        matrix = []
+
+        for csv_elem in csv_list:
+            dict_csv = {}
+            for kv in enumerate(csv_elem):
+                dict_csv[list_keys[kv[0]]] = int(kv[1])
+            matrix.append(dict_csv)
+
+        list_ins = []
+
+        for index in range(len(matrix)):
+            list_ins.append(cls.create(**matrix[index]))
+
+        return list_ins
