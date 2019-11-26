cf8373371c2938e977a3b1e00f95d9412d2ef8f5
JoseMarulanda <josejumarumol@gmail.com>
2019-11-26 07:14:10 +0000

    15 fix

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,2 +1,5 @@
 --  lists all records of the table second_table.
-SELECT score, name FROM second_table WHERE name IS NOT NULL ORDER BY score DESC;
+SELECT score, COUNT(*) AS number
+FROM second_table
+GROUP BY score
+ORDER BY score DESC;
