fbbfaa56f3121dc529b8b50dcd8ecd04c8dea343
JoseMarulanda <josejumarumol@gmail.com>
2020-01-14 04:51:37 +0000

    pep8 indentation

--- 0x11-python-network_1/9-starwars.py
+++ 0x11-python-network_1/9-starwars.py
@@ -6,9 +6,9 @@ import requests
 import sys
 
 if __name__ == "__main__":
-   search = 'https://swapi.co/api/people/?search={}'.format(sys.argv[1])
-   r = requests.get(search)
-   json_o = r.json()
-   print("Number of results: {}".format(json_o.get('count')))
-   for character in json_o.get('results'):
-       print(character.get('name'))
+    search = 'https://swapi.co/api/people/?search={}'.format(sys.argv[1])
+    r = requests.get(search)
+    json_o = r.json()
+    print("Number of results: {}".format(json_o.get('count')))
+    for character in json_o.get('results'):
+        print(character.get('name'))
