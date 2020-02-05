aa13577d73ea2ecd37798d3e72a330f769c861a2
JoseMarulanda <josejumarumol@gmail.com>
2020-02-04 23:21:26 -0500

    all finished tasks

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const str = ('C is fun');
+let x = parseInt(process.argv[2]);
+if (Number.isNaN(x)) {
+    console.log("Missing number of occurrences")
+} else {
+    for (let i = 0; i < x; i++) {
+        console.log(str);
+    }
+}
