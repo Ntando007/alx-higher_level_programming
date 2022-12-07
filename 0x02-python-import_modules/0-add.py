e37616897faa214d1729e409c42e832f728e86df
ntando007 <111117959+Ntando007@users.noreply.github.com>
2022-12-07 11:14:54 +0200

    file add

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-def add(a, b):
+if __name__ == "__main__":
+  from add_0.py import add
   a = 1
   b = 2
-  while a < b:
-    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
-    
+  print("{:d} + {:d} = {:d}" .format(a, b, add(a ,b)))
+  
