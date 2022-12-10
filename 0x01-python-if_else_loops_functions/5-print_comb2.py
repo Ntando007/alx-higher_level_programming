34cda4ad533905f2647370b2589bbeeed2a2bde6
ntando007 <111117959+Ntando007@users.noreply.github.com>
2022-12-10 14:57:58 +0200

    chaanges

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
+for i in range(0, 99):
+    print('{:d}{:d}'.format(i // 10, i % 10), end=", ")
 
-for i in range(0, 100):
-  if i != 99:
-    print("{:02d}, ".format(i), end='')
-  else:
-    print("{:02d}".format(i))
+print('99')
