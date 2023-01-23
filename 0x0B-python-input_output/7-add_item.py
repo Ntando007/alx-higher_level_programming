752151158078bf89c143b700fd7f13fa613585d7
ntando007 <111117959+Ntando007@users.noreply.github.com>
2023-01-23 12:23:42 +0200

    Update 7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -3,7 +3,6 @@
 Adds all arguments to a JSON file.
 """
 
-
 from sys import argv
 save_to_json = __import__('7-save_to_json_file').save_to_json_file
 load_from_json = __import__('8-load_from_json_file').load_from_json_file
