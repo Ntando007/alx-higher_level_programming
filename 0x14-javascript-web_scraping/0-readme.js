e2998f7de1c46c6fc61af687acdf2e56b5c9060d
ntando007 <111117959+Ntando007@users.noreply.github.com>
2023-05-03 14:43:30 +0200

    fix syntax

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -3,10 +3,10 @@
 const readfile = require('readfile');
 
 readfile.readFile(process.argv[2], 'utf-8', (err, data) => {
-if (err) {
-	console.log(err);
-}
-else {
-console.log(data);
-}
+	if (err) {
+		console.log(err);
+	}
+	else {
+		console.log(data);
+	}
 });
