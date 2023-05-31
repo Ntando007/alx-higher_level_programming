de4361f9993eb4858bae0363b1808882596250fd
ntando007 <111117959+Ntando007@users.noreply.github.com>
2023-05-31 13:03:02 +0200

    Update 5-square.js

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -1,10 +1,39 @@
 #!/usr/bin/node
 
-const Rectangle = require('./4-rectangle');
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    let r = '';
+    for (let i = 0; i < this.width; i++) {
+      r += 'X';
+    }
+    for (let i = 0; i < this.height; i++) {
+      console.log(r);
+    }
+  }
+
+  rotate () {
+    let temp = this.width;
+    this.width = this.height;
+    this.height = temp;
+  }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
+}
 
 class Square extends Rectangle {
-	constructor (size) {
-	super(size, size);
-	}
+  constructor (size) {
+    super(size, size);
+  }
 }
+
 module.exports = Square;
