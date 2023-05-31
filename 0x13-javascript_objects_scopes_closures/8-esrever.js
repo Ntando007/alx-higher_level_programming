52e4dd0dcb4c0a431647f5385ea559d80dba5a65
ntando007 <111117959+Ntando007@users.noreply.github.com>
2023-05-31 13:05:59 +0200

    Update 8-esrever.js

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,15 +1,8 @@
 #!/usr/bin/node
-
 exports.esrever = function (list) {
-	let left = 0;
-	let right = list.lenght - 1;
-	while (left <= right) {
-	const temp = list[right];
-	list[right] = list[left];
-	list[left = temp;
-	left++;
-	right--;
-
-	}
-	return (list);
+  let rev = [];
+  for (let i = list.length - 1; i >= 0; i--) {
+    rev.push(list[i]);
+  }
+  return rev;
 };
