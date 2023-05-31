06809038cab9e933be72d72871ed6845600a931b
ntando007 <111117959+Ntando007@users.noreply.github.com>
2023-05-31 13:01:14 +0200

    Update 4-rectangle.js

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -1,33 +1,31 @@
 #!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
 
-class Rectangle{
-	constructor (w, h){
-	if ((w > 0) && (h > 0)){
-		this.width = w;
-		this.height =h;
-	}
-	}
-	
-	print() {
-	for (let x = 0; x < this.height; x++){
-	let i = '';
-	for (let y = 0; y < this.width; y++) {
-	i += 'X';
-	}
-	console.log(s);
-	}
-	}
+  print () {
+    let r = '';
+    for (let i = 0; i < this.width; i++) {
+      r += 'X';
+    }
+    for (let i = 0; i < this.height; i++) {
+      console.log(r);
+    }
+  }
 
-	rotate () {
-	const aux = this.width;
-	this.width = this.height;
-	this.height = aux;
-	}
+  rotate () {
+    let temp = this.width;
+    this.width = this.height;
+    this.height = temp;
+  }
 
-	double () {
-	this.width *= 2;
-	this.height *= 2;
-	}
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
 }
-
 module.exports = Rectangle;
