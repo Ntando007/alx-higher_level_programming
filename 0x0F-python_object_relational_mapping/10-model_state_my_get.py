38e07e24c17a1b494b84c9be3b39a5b6a65f1565
ntando007 <111117959+Ntando007@users.noreply.github.com>
2023-06-07 10:42:34 +0200

    Create 10-model_state_my_get.py

---
+++ 0x0F-python_object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+
+import sys
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import Session
+from sqlalchemy.engine.url import URL
+from model_state import Base, State
+
+
+if __name__ == "__main__":
+    mySQL_u = sys.argv[1]
+    mySQL_p = sys.argv[2]
+    db_name = sys.argv[3]
+
+    st_name = sys.argv[4]
+
+    url = {'drivername': 'mysql+mysqldb', 'host': 'localhost',
+           'username': mySQL_u, 'password': mySQL_p, 'database': db_name}
+
+    engine = create_engine(URL(**url), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    session = Session(bind=engine)
+
+    q = session.query(State).filter(State.name == st_name).order_by(State.id)
+
+    if q.first():
+        print(q.first().id)
+    else:
+        print("Not found")
