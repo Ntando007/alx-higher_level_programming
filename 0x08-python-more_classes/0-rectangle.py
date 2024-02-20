7fc21142056ab611a1c238fbde0b9740ec81822f
ntando007 <111117959+Ntando007@users.noreply.github.com>
2024-02-20 11:04:09 +0200

    Create 0-rectangle.py

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# 0-rectangle.py
+# Ntando Busakwe <ntando.busakwe99@gmail.com>
+"""Defines a Rectangle class."""
+
+
+class Rectangle:
+    """Represent a rectangle."""
+    pass
